4a15d5d321247d77c7e344591f11a3cd9b9edae8
itsrafsanjani <48161278+itsrafsanjani@users.noreply.github.com>
2025-10-16 23:11:51 +0600

    chore: remove role permission helper

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TeamServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\TeamServiceProvider::class,
 ];
